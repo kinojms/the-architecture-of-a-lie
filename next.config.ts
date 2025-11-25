1013b7931f8488e3888dd58eb5b617e67172539a
tocinooo <sacdalanjm83@gmail.com>
2025-11-26 05:48:49 +0800

    base path fix

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
-
+const repoBase = "/the-architecture-of-a-lie";
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export"
+  output: "export",
+  basePath: repoBase,
+  assetPrefix: repoBase,
 };
 
 export default nextConfig;
